f40db9a9e4b79d6c179f8d7daf492a947e6f8664
anerb <anerbenartzi@gmail.com>
2023-01-04 17:35:01 +0100

    do not cache any files

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 var version = '20221221.1051';
 var cacheName = 'hello-pwa';
 var filesToCache = [
-  './index.html',
-  './css/style.css',
-  './js/main.js'
+//  './index.html',
+//  './css/style.css',
+//  './js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
