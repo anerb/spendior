46b2286622444f5825cb0869d5ecf7aaeae2f236
anerb <11691668+anerb@users.noreply.github.com>
2023-03-01 12:31:42 +0000

    manifest

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var version = '20230208.1622';
-var cacheName = 'spendior-cache-20230208.1622';
+var version = '20230301.1320';
+var cacheName = 'spendior-cache-20230301.1320';
 var filesToCache = [];
 /*
   './index.html',
