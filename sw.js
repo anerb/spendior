3e7005ae69a3a3c03376900b60bbf166df07f5c3
anerb <11691668+anerb@users.noreply.github.com>
2023-04-04 20:23:28 +0000

    enable offline maybe

--- sw.js
+++ sw.js
@@ -1,15 +1,55 @@
-var version = '20230301.1425';
-var cacheName = 'spendior-cache-20230301.1425';
+var version=202304042150;
+var cacheName = `version=${version}`;
+
+const appShellFiles = [
+  "./app/debug.html",
+  "./app/index.html",
+  "./app/settings.html",
+  "./css/settings.css",
+  "./css/style.css",
+  "./js/common.js",
+  "./js/debug.js",
+  "./js/main.js",
+  "./js/settings.js",
+  "./manifest.json",
+  "./sw.js",
+];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install',
   (e) => {
     console.log("[Service Worker] Install");
+
   }
 );
 
-self.addEventListener('activate',
-  (e) => {
-    console.log("[Service Worker] activate");
-  }
-);
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key === cacheName) {
+            return;
+          }
+          return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) {
+        return r;
+      }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      cache.put(e.request, response.clone());
+      return response;
+    })()
+  );
+});
